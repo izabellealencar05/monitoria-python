63b379a89b7732b37c27b7d613242e2a5359c1f0
izabellealencar05 <126572391+izabellealencar05@users.noreply.github.com>
2023-05-23 08:45:58 -0300

    Add files via upload

---
+++ exercicio 2.py
@@ -0,0 +1,9 @@
+#Crie um programa que receba um nome e imprima em seguida:
+#Para receber o nome: nome = input(“Qual o nome?”)
+#numero = float(input(“Qual o número? “))
+#"Seja bem vindo ao Python [ nome ] e multiplica o nome"
+
+nome = str(input("qual o seu nome? "))
+numero = int(input("qual o numero? "))
+print("seja bem vindo ao Python", nome)
+print(f'{nome * numero}')
