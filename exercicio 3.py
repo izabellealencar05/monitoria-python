aecd33f29c1a7ca6eb377a12ef1f7e079087c0be
izabellealencar05 <126572391+izabellealencar05@users.noreply.github.com>
2023-05-23 09:50:31 -0300

    Add files via upload

---
+++ exercicio 3.py
@@ -0,0 +1,15 @@
+#Receba (input) um valor em real (R$) e mostre a conversão para dólar americano e euro
+#Entrada -> reais (R$)
+#Saída -> dolar (USD) e Euro
+
+dolar = 4.97
+euro = 5.31
+
+
+real = float(input("qual o valor em real? "))
+print(f"o valor em dolar é: {real / dolar}")
+print(f"o valor em euro é: {real / euro}")
+
+
+
+
