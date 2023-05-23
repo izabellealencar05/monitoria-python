82bf3c2f60606f535e7b746ab064dce85b818ce8
izabellealencar05 <126572391+izabellealencar05@users.noreply.github.com>
2023-05-23 08:34:18 -0300

    Add files via upload

---
+++ exercicio 1.py
@@ -0,0 +1,16 @@
+#Receba (input) uma base e uma altura e calcule/mostre a área de um triângulo
+# area = (base * altura) / 2)
+
+#a) não usando input
+"""base = 10
+altura = 10
+area = (base * altura)/2
+print(area)"""
+
+#b)usando input
+base = float(input("digite o tamanho da base: "))
+altura = float(input("digite o tamanho da altura: "))
+print("a area do triangulo é:", (base*altura)/2)
+
+
+
