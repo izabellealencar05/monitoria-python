1ec00c4135f331e9ab01a956a1d6e8c3feed75d3
izabellealencar05 <126572391+izabellealencar05@users.noreply.github.com>
2023-05-24 19:51:46 -0300

    Add files via upload

---
+++ aula 1 - 5 .py
@@ -0,0 +1,9 @@
+#Desenvolva a função máxima que recebe dois números inteiros e retorna o maior deles
+
+a = int(input("digite o primeiro valor: "))
+b = int(input("digite o segundo valor: "))
+
+if a > b:
+  print(a, "é maior que ", b)
+else:
+  print(b, "é maior que ", a)
