4a768493df36f2e0ce26322bfbc467cd6758419e
izabellealencar05 <126572391+izabellealencar05@users.noreply.github.com>
2023-06-01 18:20:52 -0300

    Add files via upload

---
+++ q15.py
@@ -0,0 +1,8 @@
+# Crie uma função que recebe dois parâmetros e faz a soma,
+# produto, divisão e subtração desses parâmetros após isso
+# implemente a aplicação dessa função em outro programa e mostro
+# o resultado no segundo programa
+
+def operacao (a, b):
+    return a+b, a-b, a*b, a/b
+
---
+++ q15continuacao.py
@@ -0,0 +1,6 @@
+from q15 import operacao
+
+a = int(input("1 valor? "))
+b = int(input('2 valor? '))
+
+print(operacao(a, b))
